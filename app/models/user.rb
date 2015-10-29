37d14ad5a105d5ef272524a2e9e763ade79ca8f8
Anil Kumar <aniltimt@gmail.com>
2015-10-29 11:55:20 +0530

    more changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,24 +3,28 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   belongs_to :role
-  has_many :items, :dependent => :destroy
   validates_presence_of :name
   before_save :assign_role
 
   def assign_role
-    self.role = Role.find_by name: "Regular" if self.role.nil?
+    self.role = Role.find_by name: "patient" if self.role.nil?
+  end
+
+  def super_admin?
+    self.role.name == "super_admin"
   end
 
   def admin?
-    self.role.name == "Admin"
+    self.role.name == "admin"
   end
 
-  def seller?
-    self.role.name == "Seller"
+  def dietitian?
+    self.role.name == "dietitian"
   end
 
-  def regular?
-    self.role.name == "Regular"
+  def patient?
+    self.role.name == "patient"
   end
 end
