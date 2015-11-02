111e86df69f32a474e38fcd7132add913ba36d61
Nitesh <nitesh@mobikasa.com>
2015-11-02 12:54:18 +0530

    fix

--- app/controllers/super_admins_controller.rb
+++ app/controllers/super_admins_controller.rb
@@ -1,8 +1,10 @@
 class SuperAdminsController < ApplicationController
   #load_and_authorize_resource
   def dashboard
-  	#@q = User.all.ransack(params[:q])
-  	@users = User.all.page params[:page]
+
+  	@q = User.all.ransack(params[:q])
+  	@users = @q.result.page params[:page]
   	@dietitians = User.where(:role_id => 3)
+
   end
 end
